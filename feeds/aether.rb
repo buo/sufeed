7b453567c0e23c831ea5217ff4903db269acf1a5
buo <buo@users.noreply.github.com>
2015-09-27 01:47:18 +0900

    Add aether

---
+++ feeds/aether.rb
@@ -0,0 +1,8 @@
+desc 'aether' do
+  items = github('https://github.com/nehbit/aether-public/releases.atom')
+  items = items.select { |item| item[:id].match(/-OSX$/) }
+  latest = items.first
+  version = File.basename(latest[:link]).delete('v').delete('-OSX')
+  url = "https://github.com/nehbit/aether-public/releases/download/v#{version}-OSX/Aether.#{version}.dmg"
+  {version => url}
+end
