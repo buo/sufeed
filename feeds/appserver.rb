ff9f54c065b7604266367f68645f2087c8b0c4a0
buo <buo@users.noreply.github.com>
2015-09-29 21:42:21 +0900

    appserver: use github api

--- feeds/appserver.rb
+++ feeds/appserver.rb
@@ -1,8 +1,6 @@
 desc 'appserver' do
-  items = github('https://github.com/appserver-io/appserver/releases.atom')
-  items = items.select { |item| item[:tag].match(/^[\d\.]+$/) }
-  latest = items.first
-  version = latest[:tag]
-  url = "https://github.com/appserver-io/appserver/releases/download/#{version}/appserver-dist_#{version}-21_x86_64.pkg"
+  latest = github('appserver-io', 'appserver').first
+  version = latest.tag_name
+  url = latest.assets.select { |e| e.name.match(/\.pkg/) }.first.browser_download_url
   {version => url}
 end
