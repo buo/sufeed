5849936341be744873192699514091499b8f439e
buo <buo@users.noreply.github.com>
2015-09-29 22:23:16 +0900

    Add boot2docker-status

---
+++ feeds/boot2docker-status.rb
@@ -0,0 +1,6 @@
+desc 'boot2docker-status' do
+  latest = github('nickgartmann', 'boot2docker-status').first
+  version = latest.tag_name
+  url = latest.assets.first.browser_download_url
+  {version => url}
+end
