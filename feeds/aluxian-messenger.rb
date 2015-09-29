43f183b024be3b2c3c4c169daf43aa5d3fc062b2
buo <buo@users.noreply.github.com>
2015-09-29 21:41:53 +0900

    aluxian-messenger: use github api

--- feeds/aluxian-messenger.rb
+++ feeds/aluxian-messenger.rb
@@ -1,6 +1,6 @@
 desc 'aluxian-messenger' do
   latest = github('Aluxian', 'Facebook-Messenger-Desktop').first
-  version = latest.tag_name.delete('v')
+  version = latest.tag_name.gsub(/v/, '')
   package = latest.assets.select { |asset| asset.name.match(/\.dmg/) }.first
   url = package.browser_download_url
   {version => url}
