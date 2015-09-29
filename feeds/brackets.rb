89e9c5bbf94a80529011ad04639f8ea71910d3c7
buo <buo@users.noreply.github.com>
2015-09-29 22:26:10 +0900

    Add brackets

---
+++ feeds/brackets.rb
@@ -0,0 +1,6 @@
+desc 'brackets' do
+  latest = github('adobe', 'brackets').select { |e| e.tag_name.match(/^release-[\d\.]+$/) }.first
+  version = latest.tag_name.gsub(/release-/, '')
+  url = latest.assets.select { |e| e.name.match(/\.dmg$/) }.first.browser_download_url
+  {version => url}
+end
