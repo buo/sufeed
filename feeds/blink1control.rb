57c2d5d71cca432b3259d89bba28fd97611a6a2d
buo <buo@users.noreply.github.com>
2015-09-29 22:19:39 +0900

    Add blink1control

---
+++ feeds/blink1control.rb
@@ -0,0 +1,6 @@
+desc 'blink1control' do
+  latest = github('todbot', 'blink1').first
+  version = latest.tag_name.gsub(/v/, '')
+  url = latest.assets.select { |e| e.name.match(/mac\.zip/) }.first.browser_download_url
+  {version => url}
+end
