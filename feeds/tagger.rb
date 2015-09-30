7955ebeb3dd083c6190714df39d03965c4098531
buo <buo@users.noreply.github.com>
2015-09-30 18:43:04 +0900

    Add tagger

---
+++ feeds/tagger.rb
@@ -0,0 +1,7 @@
+desc 'tagger' do
+  latest = github('Bilalh', 'Tagger').first
+  package = latest.assets.first
+  version = package.name.match(/([\d\.]+)\.zip/)[1]
+  url = package.browser_download_url
+  {version => url}
+end
