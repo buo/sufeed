0b8b1747b0d79edd6572144fc865b904f07ff877
buo <buo@users.noreply.github.com>
2015-09-30 19:51:37 +0900

    Add clementine

---
+++ feeds/clementine.rb
@@ -0,0 +1,10 @@
+desc 'clementine' do
+  items = github('clementine-player', 'Clementine')
+  items = items.select do |item|
+    !item.assets.index { |a| a.name.match(/dmg/) }.nil?
+  end
+  latest = items.first
+  version = latest.tag_name
+  url = latest.assets.select { |e| e.name.match(/dmg/) }.first.browser_download_url
+  {version => url}
+end
