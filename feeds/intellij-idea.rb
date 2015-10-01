2d37e5527aa657ec0517e4f35c81a8c16a4ee62f
buo <buo@users.noreply.github.com>
2015-10-01 19:49:14 +0900

    intellij-idea.rb: use hashie

--- feeds/intellij-idea.rb
+++ feeds/intellij-idea.rb
@@ -2,5 +2,5 @@ desc 'intellij-idea' do
   resp = text('https://www.jetbrains.com/js2/version.js')
   version = resp.match(/var versionIDEALong = "([\d\.]+)";/)[1]
   url = "https://download.jetbrains.com/idea/ideaIU-#{version}.dmg"
-  {version => url}
+  Latest.new(version: version, url: url)
 end
