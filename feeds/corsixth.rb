7941f5df54f399d0aa3ca2f79dde1dacfb72be79
buo <buo@users.noreply.github.com>
2015-10-01 19:49:52 +0900

    corsixth.rb: use hashie

--- feeds/corsixth.rb
+++ feeds/corsixth.rb
@@ -5,5 +5,5 @@ desc 'corsixth' do
   latest = items.first
   version = latest.tag_name.gsub(/v/, '')
   url = latest.assets.select { |e| e.name.match(/dmg/) }.first.browser_download_url
-  {version => url}
+  Latest.new(version: version, url: url)
 end
