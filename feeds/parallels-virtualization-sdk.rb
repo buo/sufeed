6d99e3750bb806814fa7b90867a09aab39d4d786
buo <buo@users.noreply.github.com>
2015-10-01 19:47:45 +0900

    parallels-virtualization-sdk.rb: use hashie

--- feeds/parallels-virtualization-sdk.rb
+++ feeds/parallels-virtualization-sdk.rb
@@ -1,5 +1,5 @@
 desc 'parallels-virtualization-sdk' do
   url = location('http://www.parallels.com/download/pvsdk/')
   version = url.match(/ParallelsVirtualizationSDK-([\d\.\-]+)-mac.dmg/)[1]
-  {version => url}
+  Latest.new(version: version, url: url)
 end
