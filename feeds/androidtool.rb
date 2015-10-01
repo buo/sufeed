de1f9149b6e127b056c765f2a6483d7f04dc702f
buo <buo@users.noreply.github.com>
2015-10-01 19:54:34 +0900

    androidtool.rb: use hashie

--- feeds/androidtool.rb
+++ feeds/androidtool.rb
@@ -2,5 +2,5 @@ desc 'androidtool' do
   latest = github('mortenjust', 'androidtool-mac').first
   version = latest.tag_name
   url = latest.assets.first.browser_download_url
-  {version => url}
+  Latest.new(version: version, url: url)
 end
