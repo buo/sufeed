0a7b18fffd39472d3fc496d4f3e2c37983e6414c
buo <buo@users.noreply.github.com>
2015-10-01 19:53:29 +0900

    backuploupe.rb: use hashie

--- feeds/backuploupe.rb
+++ feeds/backuploupe.rb
@@ -2,5 +2,5 @@ desc 'backuploupe' do
   latest = sparkle('http://www.soma-zone.com/BackupLoupe/a/appcast.xml').last
   version = latest[:enclosure][:shortVersionString]
   url = latest[:enclosure][:url]
-  {version => url}
+  Latest.new(version: version, url: url)
 end
