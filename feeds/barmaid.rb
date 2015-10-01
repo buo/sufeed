04e2eb2414c82ba208d660f1fa0a62cdc513ec39
buo <buo@users.noreply.github.com>
2015-10-01 19:53:14 +0900

    barmaid.rb: use hashie

--- feeds/barmaid.rb
+++ feeds/barmaid.rb
@@ -2,5 +2,5 @@ desc 'barmaid' do
   latest = github('zenonas', 'barmaid', :prerelease => true).first
   version = latest.tag_name.gsub(/v/, '')
   url = latest.assets.first.browser_download_url
-  {version => url}
+  Latest.new(version: version, url: url)
 end
