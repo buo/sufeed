71da33119838e3a292d4226b64e57fe1c1819d46
buo <buo@users.noreply.github.com>
2015-10-01 19:01:09 +0900

    theine.rb: use hashie

--- feeds/theine.rb
+++ feeds/theine.rb
@@ -2,5 +2,5 @@ desc 'theine' do
   latest = github('lvillani', 'theine').first
   version = latest.tag_name.gsub(/v/, '')
   url = latest.assets.first.browser_download_url
-  {version => url}
+  Latest.new(version: version, url: url)
 end
