f8347636f827b3305460331e5e1f5181a9d63c81
buo <buo@users.noreply.github.com>
2015-10-01 19:47:56 +0900

    parallels-desktop.rb: use hashie

--- feeds/parallels-desktop.rb
+++ feeds/parallels-desktop.rb
@@ -1,5 +1,5 @@
 desc 'parallels-desktop' do
   url = location('http://www.parallels.com/directdownload/pd11/')
   version = url.match(/ParallelsDesktop-([\d\.\-]+).dmg/)[1]
-  {version => url}
+  Latest.new(version: version, url: url)
 end
