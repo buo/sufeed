1fc08eab22f73a2da756ea3a5b87e3f7b662bdf9
buo <buo@users.noreply.github.com>
2015-10-01 19:52:25 +0900

    blink1control.rb: use hashie

--- feeds/blink1control.rb
+++ feeds/blink1control.rb
@@ -2,5 +2,5 @@ desc 'blink1control' do
   latest = github('todbot', 'blink1').first
   version = latest.tag_name.gsub(/v/, '')
   url = latest.assets.select { |e| e.name.match(/mac\.zip/) }.first.browser_download_url
-  {version => url}
+  Latest.new(version: version, url: url)
 end
