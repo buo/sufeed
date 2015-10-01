163666d02133dffb4071197305cbb5c4c6e22e99
buo <buo@users.noreply.github.com>
2015-10-01 19:53:56 +0900

    atom.rb: use hashie

--- feeds/atom.rb
+++ feeds/atom.rb
@@ -2,5 +2,5 @@ desc 'atom' do
   latest = github('atom', 'atom').first
   version = latest.tag_name.gsub(/v/, '')
   url = latest.assets.select { |e| e.name.match(/atom-mac\.zip/) }.first.browser_download_url
-  {version => url}
+  Latest.new(version: version, url: url)
 end
