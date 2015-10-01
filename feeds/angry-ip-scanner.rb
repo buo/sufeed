a0a4cea293a516f0ca824dcebe3408374c57069c
buo <buo@users.noreply.github.com>
2015-10-01 19:54:28 +0900

    angry-ip-scanner.rb: use hashie

--- feeds/angry-ip-scanner.rb
+++ feeds/angry-ip-scanner.rb
@@ -2,5 +2,5 @@ desc 'angry-ip-scanner' do
   latest = github('angryziber', 'ipscan').first
   version = latest.tag_name
   url = latest.assets.select { |e| e.name.match(/mac/) }.first.browser_download_url
-  {version => url}
+  Latest.new(version: version, url: url)
 end
