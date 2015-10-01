b925542d9a56804b0ea0d3f97cbf9b820661d67a
buo <buo@users.noreply.github.com>
2015-10-01 19:51:24 +0900

    calibre.rb: use hashie

--- feeds/calibre.rb
+++ feeds/calibre.rb
@@ -2,5 +2,5 @@ desc 'calibre' do
   latest = github('kovidgoyal', 'calibre').first
   version = latest.tag_name.gsub(/v/, '')
   url = latest.assets.select { |e| e.name.match(/dmg/) }.first.browser_download_url
-  {version => url}
+  Latest.new(version: version, url: url)
 end
