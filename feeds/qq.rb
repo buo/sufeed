d7fcc980e62b0bc8f1caa688e2af864da48858fd
buo <buo@users.noreply.github.com>
2015-10-01 19:47:28 +0900

    qq.rb: use hashie

--- feeds/qq.rb
+++ feeds/qq.rb
@@ -2,5 +2,5 @@ desc 'qq' do
   doc = html('http://im.qq.com/macqq/')
   url = doc.css('script').to_s.match(/newpkg : "(.*)",/)[1]
   version = url.match(/QQ_V([\d\.]+)\.dmg/)[1]
-  {version => url}
+  Latest.new(version: version, url: url)
 end
