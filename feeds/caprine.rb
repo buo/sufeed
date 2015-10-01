79b52579a2a78636ebdfe288f3e63a0cfc642c4d
buo <buo@users.noreply.github.com>
2015-10-01 19:51:18 +0900

    caprine.rb: use hashie

--- feeds/caprine.rb
+++ feeds/caprine.rb
@@ -2,5 +2,5 @@ desc 'caprine' do
   latest = github('sindresorhus', 'caprine').first
   version = latest.tag_name
   url = latest.assets.select { |e| e.name.match(/osx/) }.first.browser_download_url
-  {version => url}
+  Latest.new(version: version, url: url)
 end
