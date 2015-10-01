40e2b7e0f2f59770a12b9aec33dc06a6b354aa88
buo <buo@users.noreply.github.com>
2015-10-01 19:53:00 +0900

    beaconscanner.rb: use hashie

--- feeds/beaconscanner.rb
+++ feeds/beaconscanner.rb
@@ -2,5 +2,5 @@ desc 'beaconscanner' do
   latest = github('mlwelles', 'BeaconScanner').first
   version = latest.tag_name
   url = latest.assets.first.browser_download_url
-  {version => url}
+  Latest.new(version: version, url: url)
 end
