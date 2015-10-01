4829d6252ed414f3feec6ae329c410fda054336c
buo <buo@users.noreply.github.com>
2015-10-01 19:52:37 +0900

    bilibili.rb: use hashie

--- feeds/bilibili.rb
+++ feeds/bilibili.rb
@@ -2,5 +2,5 @@ desc 'bilibili' do
   latest = github('typcn', 'bilibili-mac-client').first
   version = latest.tag_name
   url = latest.assets.first.browser_download_url
-  {version => url}
+  Latest.new(version: version, url: url)
 end
