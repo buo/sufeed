23397750fad01df7e79a8f70251a807afa54bb22
buo <buo@users.noreply.github.com>
2015-10-01 19:53:05 +0900

    barxtemp.rb: use hashie

--- feeds/barxtemp.rb
+++ feeds/barxtemp.rb
@@ -2,5 +2,5 @@ desc 'barxtemp' do
   latest = github('Gabriele91', 'barXtemp').first
   version = latest.tag_name
   url = latest.assets.first.browser_download_url
-  {version => url}
+  Latest.new(version: version, url: url)
 end
