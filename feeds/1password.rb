447dd70ee358aa8cfdf950c48c885a7c91e50ee9
buo <buo@users.noreply.github.com>
2015-10-01 19:55:07 +0900

    1password.rb: use hashie

--- feeds/1password.rb
+++ feeds/1password.rb
@@ -2,5 +2,5 @@ desc '1password' do
   doc = html('https://agilebits.com/onepassword')
   url = doc.at_css('.downloadbutton')[:href]
   version = url.match(/1Password-([\d\.]+).zip/)[1]
-  {version => url}
+  Latest.new(version: version, url: url)
 end
