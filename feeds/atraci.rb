9cfbe0fd6b23656f675fd03ce8d53192e9d3318e
buo <buo@users.noreply.github.com>
2015-10-01 19:53:53 +0900

    atraci.rb: use hashie

--- feeds/atraci.rb
+++ feeds/atraci.rb
@@ -2,5 +2,5 @@ desc 'atraci' do
   latest = github('Atraci', 'Atraci').first
   version = latest.tag_name
   url = latest.assets.select { |e| e.name.match(/mac/) }.first.browser_download_url
-  {version => url}
+  Latest.new(version: version, url: url)
 end
