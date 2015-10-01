aadb91da5c815a67ac1077869b21b9b1f54aabb9
buo <buo@users.noreply.github.com>
2015-10-01 19:52:09 +0900

    boot2docker-status.rb: use hashie

--- feeds/boot2docker-status.rb
+++ feeds/boot2docker-status.rb
@@ -2,5 +2,5 @@ desc 'boot2docker-status' do
   latest = github('nickgartmann', 'boot2docker-status').first
   version = latest.tag_name
   url = latest.assets.first.browser_download_url
-  {version => url}
+  Latest.new(version: version, url: url)
 end
