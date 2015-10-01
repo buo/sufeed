24c792e37329b4d0c9f80c1d7b97848600529c84
buo <buo@users.noreply.github.com>
2015-10-01 19:50:01 +0900

    coreos-osx-gui.rb: use hashie

--- feeds/coreos-osx-gui.rb
+++ feeds/coreos-osx-gui.rb
@@ -2,5 +2,5 @@ desc 'coreos-osx-gui' do
   latest = github('rimusz', 'coreos-osx-gui').first
   version = latest.tag_name.gsub(/v/, '')
   url = latest.assets.first.browser_download_url
-  {version => url}
+  Latest.new(version: version, url: url)
 end
