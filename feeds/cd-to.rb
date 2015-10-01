b5bab1652c3783588b0ea36aa837733c0ed8270d
buo <buo@users.noreply.github.com>
2015-10-01 19:51:06 +0900

    cd-to.rb: use hashie

--- feeds/cd-to.rb
+++ feeds/cd-to.rb
@@ -2,5 +2,5 @@ desc 'cd-to' do
   latest = github('jbtule', 'cdto').first
   version = latest.tag_name.gsub(/_/, '.')
   url = latest.assets.first.browser_download_url
-  {version => url}
+  Latest.new(version: version, url: url)
 end
