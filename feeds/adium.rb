3f946b854d6a62ccd9a2cf64fdfd74e4bc22c9f3
buo <buo@users.noreply.github.com>
2015-10-01 19:54:55 +0900

    adium.rb: use hashie

--- feeds/adium.rb
+++ feeds/adium.rb
@@ -2,5 +2,5 @@ desc 'adium' do
   latest = sparkle('https://www.adium.im/sparkle/appcast-release.xml').first
   version = latest[:enclosure][:version]
   url = latest[:enclosure][:url]
-  {version => url}
+  Latest.new(version: version, url: url)
 end
