11d1c8085e5b4146cd75de025356b9aee1fd0a03
buo <buo@users.noreply.github.com>
2015-10-01 19:51:37 +0900

    backets.rb: use hashie

--- feeds/brackets.rb
+++ feeds/brackets.rb
@@ -2,5 +2,5 @@ desc 'brackets' do
   latest = github('adobe', 'brackets').select { |e| e.tag_name.match(/^release-[\d\.]+$/) }.first
   version = latest.tag_name.gsub(/release-/, '')
   url = latest.assets.select { |e| e.name.match(/\.dmg$/) }.first.browser_download_url
-  {version => url}
+  Latest.new(version: version, url: url)
 end
