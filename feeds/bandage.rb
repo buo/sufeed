b5f9f50b98dffad607056e87d5475ac9ff1b4cbc
buo <buo@users.noreply.github.com>
2015-10-01 19:53:23 +0900

    bandage.rb: use hashie

--- feeds/bandage.rb
+++ feeds/bandage.rb
@@ -2,5 +2,5 @@ desc 'bandage' do
   latest = github('rrwick', 'Bandage').first
   version = latest.tag_name.gsub(/^v/, '')
   url = latest.assets.first.browser_download_url
-  {version => url}
+  Latest.new(version: version, url: url)
 end
