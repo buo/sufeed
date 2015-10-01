00f0eb533afd6fdf268878ee06b7b9acbb0c429e
buo <buo@users.noreply.github.com>
2015-10-01 19:51:58 +0900

    boot2docker: use hashie

--- feeds/boot2docker.rb
+++ feeds/boot2docker.rb
@@ -2,5 +2,5 @@ desc 'boot2docker' do
   latest = github('boot2docker', 'osx-installer').first
   version = latest.tag_name.gsub(/v/, '')
   url = latest.assets.first.browser_download_url
-  {version => url}
+  Latest.new(version: version, url: url)
 end
