c94048e539de98446c5dc437119dc29dc9516dd0
buo <buo@users.noreply.github.com>
2015-10-01 19:52:54 +0900

    beardedspice.rb: use hashie

--- feeds/beardedspice.rb
+++ feeds/beardedspice.rb
@@ -2,5 +2,5 @@ desc 'beardedspice' do
   latest = github('beardedspice', 'beardedspice').first
   version = latest.tag_name.gsub(/v/, '')
   url = latest.assets.first.browser_download_url
-  {version => url}
+  Latest.new(version: version, url: url)
 end
