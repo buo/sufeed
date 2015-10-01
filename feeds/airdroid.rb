dd00cecb6420620c86c32128da7f5a38c79b1d52
buo <buo@users.noreply.github.com>
2015-10-01 19:54:46 +0900

    airdroid.rb: use hashie

--- feeds/airdroid.rb
+++ feeds/airdroid.rb
@@ -2,5 +2,5 @@ desc 'airdroid' do
   url = jsondata('https://srv3.airdroid.com/p14/web/getbinary?type=dmg')['data']
   m = url.match(/AirDroid_Desktop_Client_([\d\.]+).dmg/)
   version = m[1]
-  {version => url}
+  Latest.new(version: version, url: url)
 end
