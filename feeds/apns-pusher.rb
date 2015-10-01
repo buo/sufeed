f45b4b3247fd556271aae42f4afb7d0dd3423dde
buo <buo@users.noreply.github.com>
2015-10-01 19:54:17 +0900

    apns-pusher.rb: use hashie

--- feeds/apns-pusher.rb
+++ feeds/apns-pusher.rb
@@ -2,5 +2,5 @@ desc 'apns-pusher' do
   latest = github('KnuffApp', 'APNS-Pusher').first
   version = latest.tag_name.gsub(/v/, '')
   url = latest.assets.first.browser_download_url
-  {version => url}
+  Latest.new(version: version, url: url)
 end
