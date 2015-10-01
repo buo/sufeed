31690f1aaed55376c661b12b4a2e1be80a2ef8ad
buo <buo@users.noreply.github.com>
2015-10-01 19:49:24 +0900

    hub.rb: use hashie

--- feeds/hub.rb
+++ feeds/hub.rb
@@ -2,5 +2,5 @@ desc 'hub' do
   latest = github('github', 'hub').first
   version = latest.tag_name.gsub(/^v/, '')
   url = latest.assets.select { |e| e.name.match(/mac/) }.first.browser_download_url
-  {version => url}
+  Latest.new(version: version, url: url)
 end
