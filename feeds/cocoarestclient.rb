d0f9ea4bfe03ee25e72eb0b7fcf4b57aede996ea
buo <buo@users.noreply.github.com>
2015-10-01 19:50:27 +0900

    cocoarestclient.rb: use hashie

--- feeds/cocoarestclient.rb
+++ feeds/cocoarestclient.rb
@@ -2,5 +2,5 @@ desc 'cocoarestclient' do
   latest = github('mmattozzi', 'cocoa-rest-client').first
   version = latest.tag_name
   url = latest.assets.first.browser_download_url
-  {version => url}
+  Latest.new(version: version, url: url)
 end
