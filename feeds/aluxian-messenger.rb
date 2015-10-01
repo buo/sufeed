1c6ea449b80a3b9467641de77c693cb441fbb4de
buo <buo@users.noreply.github.com>
2015-10-01 19:54:41 +0900

    aluxian-messenger.rb: use hashie

--- feeds/aluxian-messenger.rb
+++ feeds/aluxian-messenger.rb
@@ -3,5 +3,5 @@ desc 'aluxian-messenger' do
   version = latest.tag_name.gsub(/v/, '')
   package = latest.assets.select { |asset| asset.name.match(/\.dmg/) }.first
   url = package.browser_download_url
-  {version => url}
+  Latest.new(version: version, url: url)
 end
