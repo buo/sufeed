cd7e3467cafea0a5802aec3b6bb5a29a286980bd
buo <buo@users.noreply.github.com>
2015-10-01 19:52:14 +0900

    bob.rb: use hashie

--- feeds/bob.rb
+++ feeds/bob.rb
@@ -2,5 +2,5 @@ desc 'bob' do
   latest = github('casperstorm', 'Bob', :prerelease => true).first
   version = latest.tag_name
   url = latest.assets.first.browser_download_url
-  {version => url}
+  Latest.new(version: version, url: url)
 end
