84607dd7f29dc15d00d00aa3e3e689ca9432e20f
buo <buo@users.noreply.github.com>
2015-10-01 19:01:52 +0900

    teitoku.rb: use hashie

--- feeds/teitoku.rb
+++ feeds/teitoku.rb
@@ -2,5 +2,5 @@ desc 'teitoku' do
   latest = github('geta6', 'teitoku').first
   version = latest.tag_name
   url = latest.assets.select { |e| e.name.match(/osx/) }.first.browser_download_url
-  {version => url}
+  Latest.new(version: version, url: url)
 end
