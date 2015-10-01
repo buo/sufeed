a532af5bae8425b731d505bf262ca74a963988ce
buo <buo@users.noreply.github.com>
2015-10-01 19:00:54 +0900

    tiddlywiki.rb: use hashie

--- feeds/tiddlywiki.rb
+++ feeds/tiddlywiki.rb
@@ -2,5 +2,5 @@ desc 'tiddlywiki' do
   latest = github('Jermolene', 'TiddlyDesktop').first
   version = latest.tag_name.gsub(/v/, '')
   url = latest.assets.select { |e| e.name.match(/mac64/) }.first.browser_download_url
-  {version => url}
+  Latest.new(version: version, url: url)
 end
