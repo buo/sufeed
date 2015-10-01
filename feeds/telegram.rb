99156f85a5b86faf1cf6d05a06715df34776c45c
buo <buo@users.noreply.github.com>
2015-10-01 19:03:02 +0900

    telegram.rb: use hashie

--- feeds/telegram.rb
+++ feeds/telegram.rb
@@ -1,5 +1,5 @@
 desc 'telegram' do
   url = location('https://tdesktop.com/mac/')
   version = url.match(/tsetup\.([\d\.]+)\.dmg/)[1]
-  {version => url}
+  Latest.new(version: version, url: url)
 end
