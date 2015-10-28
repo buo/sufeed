76f7a23856d420c547bb4f3e106e1fbce4dec4a5
buo <buo@users.noreply.github.com>
2015-10-29 02:16:30 +0900

    cdock: update

--- feeds/cdock.rb
+++ feeds/cdock.rb
@@ -1,6 +1,6 @@
 desc 'cdock' do
   latest = github('w0lfschild', 'cDock').first
-  version = latest.tag_name.gsub(/v/, '')
+  version = latest.tag_name.gsub(/cDock2-/, '')
   url = latest.assets.first.browser_download_url
   Latest.new(version: version, url: url)
 end
