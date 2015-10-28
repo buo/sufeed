85810a8e8f467cd850b3b989268d69b2982b47e5
buo <buo@users.noreply.github.com>
2015-10-29 02:19:46 +0900

    eloquent: parse official web page instead of appcast

--- feeds/eloquent.rb
+++ feeds/eloquent.rb
@@ -1,6 +1,8 @@
 desc 'eloquent' do
-  item = sparkle('http://www.eloquent-bible-study.eu/Eloquent_L2_Appcast.xml').first
-  version = item.title.match(/Version ([\d\.]+)-\d+/)[1]
-  url = item.enclosure.url
+  # The latest in the appcast is not a stable version.
+  # item = sparkle('http://www.eloquent-bible-study.eu/Eloquent_L2_Appcast.xml').first
+  doc = html('https://launchpad.net/eloquent')
+  version = doc.at_css('#downloads .version').text.strip.gsub(/Latest version is/, '').strip
+  url = doc.at_css('#downloads .version +ul li a')[:href]
   Latest.new(version: version, url: url)
 end
