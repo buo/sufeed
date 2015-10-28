bdcbb1b06f6903cc1515ca02807dab4f12d80b87
buo <buo@users.noreply.github.com>
2015-10-29 02:18:46 +0900

    consul-template: check whether assets are empty or not

--- feeds/consul-template.rb
+++ feeds/consul-template.rb
@@ -1,6 +1,7 @@
 desc 'consul-template' do
   latest = github('hashicorp', 'consul-template').first
   version = latest.tag_name.gsub(/v/, '')
+  return nil if latest.assets.empty?
   url = latest.assets.select { |e| e.name.match(/darwin_amd64/) }.first.browser_download_url
   Latest.new(version: version, url: url)
 end
