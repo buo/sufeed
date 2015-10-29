50e6d194a1e30558c17164b8f45771028fd630e2
buo <buo@users.noreply.github.com>
2015-10-30 03:01:01 +0900

    propresenter: change the version format

--- feeds/propresenter.rb
+++ feeds/propresenter.rb
@@ -2,7 +2,7 @@ desc 'propresenter' do
   item = sparkle('https://www.renewedvision.com/update/ProPresenter6.php').first
   version = item.enclosure.shortVersionString
   build = item.enclosure.version
-  version = "#{version}-#{build}"
+  version = "#{version}_b#{build}"
   url = item.enclosure.url
   Latest.new(version: version, url: url)
 end
