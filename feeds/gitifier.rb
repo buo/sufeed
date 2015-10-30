165f15e2eeb7a716af38bd02bfb0ff6f8763477f
buo <buo@users.noreply.github.com>
2015-10-30 22:31:03 +0900

    Add gitifier

---
+++ feeds/gitifier.rb
@@ -0,0 +1,6 @@
+desc 'gitifier' do
+  latest = github('nschum', 'Gitifier').first
+  version = latest.tag_name.gsub(/^v/, '')
+  url = latest.assets.first.browser_download_url
+  Latest.new version: version, url: url
+end
