51a2074bc4c29bb513e68caeeb98d39fcb34d43e
buo <buo@users.noreply.github.com>
2015-10-30 20:17:15 +0900

    Update templates

--- templates/github.rb
+++ templates/github.rb
@@ -1,7 +1,7 @@
 desc '{{NAME}}' do
   latest = github('{{OWNER}}', '{{REPO}}').first
-  version = latest.tag_name
+  version = latest.tag_name.gsub(/^v/, '')
   latest.assets.each { |e| puts e.name }
   url = latest.assets.first.browser_download_url
-  Latest.new(version: version, url: url)
+  Latest.new version: version, url: url
 end
--- templates/sparkle.rb
+++ templates/sparkle.rb
@@ -2,5 +2,5 @@ desc '{{NAME}}' do
   item = sparkle('{{URL}}').first
   version = item.enclosure.shortVersionString
   url = item.enclosure.url
-  Latest.new(version: version, url: url)
+  Latest.new version: version, url: url
 end
