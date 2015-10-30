54858518ce96ed53d60ebec8a6a2597f0b36c73e
buo <buo@users.noreply.github.com>
2015-10-30 20:14:50 +0900

    Add aerial

---
+++ feeds/aerial.rb
@@ -0,0 +1,6 @@
+desc 'aerial' do
+  latest = github('JohnCoates', 'Aerial').first
+  version = latest.tag_name.gsub(/^v/, '')
+  url = latest.assets.first.browser_download_url
+  Latest.new version: version, url: url
+end
