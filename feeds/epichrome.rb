ca7797039748a6486b59b03f1b4da696c3a6aad7
buo <buo@users.noreply.github.com>
2015-10-30 20:56:01 +0900

    Add epichrome

---
+++ feeds/epichrome.rb
@@ -0,0 +1,6 @@
+desc 'epichrome' do
+  latest = github('dmarmor', 'epichrome').first
+  version = latest.tag_name.gsub(/^v/, '')
+  url = latest.assets.first.browser_download_url
+  Latest.new version: version, url: url
+end
