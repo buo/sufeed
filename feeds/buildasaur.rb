7c65ec93ea170135b0d8958b13cb75ccfa3d6ab8
buo <buo@users.noreply.github.com>
2015-10-30 20:22:33 +0900

    Add buildasaur

---
+++ feeds/buildasaur.rb
@@ -0,0 +1,6 @@
+desc 'buildasaur' do
+  latest = github('czechboy0', 'Buildasaur').first
+  version = latest.tag_name.gsub(/^v/, '')
+  url = latest.assets.first.browser_download_url
+  Latest.new version: version, url: url
+end
