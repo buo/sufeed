e7f3b0353322e399600f92f51a104623b89fdce9
buo <buo@users.noreply.github.com>
2015-10-30 20:33:54 +0900

    Add ctivo

---
+++ feeds/ctivo.rb
@@ -0,0 +1,6 @@
+desc 'ctivo' do
+  latest = github('dscottbuch', 'cTiVo').first
+  version = latest.tag_name.gsub(/^v/, '')
+  url = latest.assets.first.browser_download_url
+  Latest.new version: version, url: url
+end
