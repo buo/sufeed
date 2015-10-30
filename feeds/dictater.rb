4b003e3f211f4f8a7b9f3344c3549f456fa10c3c
buo <buo@users.noreply.github.com>
2015-10-30 20:40:08 +0900

    Add dictater

---
+++ feeds/dictater.rb
@@ -0,0 +1,6 @@
+desc 'dictater' do
+  latest = github('Nosrac', 'Dictater').first
+  version = latest.tag_name.gsub(/^v/, '')
+  url = latest.assets.first.browser_download_url
+  Latest.new version: version, url: url
+end
