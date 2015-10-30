5270215f6b7958831bb5bac35fe4e2b9945e3e99
buo <buo@users.noreply.github.com>
2015-10-30 22:26:35 +0900

    Add ginbox

---
+++ feeds/ginbox.rb
@@ -0,0 +1,6 @@
+desc 'ginbox' do
+  latest = github('chenasraf', 'gInbox').first
+  version = latest.tag_name.gsub(/^v/, '')
+  url = latest.assets.first.browser_download_url
+  Latest.new version: version, url: url
+end
