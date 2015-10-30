00ea304b30ac871781dfe7144e5e378d89287482
buo <buo@users.noreply.github.com>
2015-10-30 22:20:51 +0900

    Add fontforge

---
+++ feeds/fontforge.rb
@@ -0,0 +1,7 @@
+desc 'fontforge' do
+  latest = github('fontforge', 'fontforge').first
+  version = latest.tag_name.gsub(/^v/, '')
+  latest.assets.select! { |e| e.name.match(/\.dmg$/) }
+  url = latest.assets.first.browser_download_url
+  Latest.new version: version, url: url
+end
