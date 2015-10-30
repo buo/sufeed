587006df3dc4f19cfdac941dda06c7cb4311aadf
buo <buo@users.noreply.github.com>
2015-10-30 15:09:58 +0900

    hub: update

--- feeds/hub.rb
+++ feeds/hub.rb
@@ -1,6 +1,7 @@
 desc 'hub' do
   latest = github('github', 'hub').first
   version = latest.tag_name.gsub(/^v/, '')
-  url = latest.assets.select { |e| e.name.match(/mac/) }.first.browser_download_url
+  asset = latest.assets.select { |e| e.name.match(/darwin/) }.first
+  url = asset.browser_download_url
   Latest.new(version: version, url: url)
 end
