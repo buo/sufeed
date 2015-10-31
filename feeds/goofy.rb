54cfa440cd9d496dc5de459fd7490e99021f5408
buo <buo@users.noreply.github.com>
2015-10-31 11:43:07 +0900

    Add goofy

---
+++ feeds/goofy.rb
@@ -0,0 +1,6 @@
+desc 'goofy' do
+  latest = github('danielbuechele', 'goofy').first
+  version = latest.tag_name.gsub(/^v/, '')
+  url = latest.assets.first.browser_download_url
+  Latest.new version: version, url: url
+end
