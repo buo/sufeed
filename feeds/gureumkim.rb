1a1eb3702bdaad092d5124607c8989d1102dd90d
buo <buo@users.noreply.github.com>
2015-11-01 17:00:32 +0900

    gureumkim: update

--- feeds/gureumkim.rb
+++ feeds/gureumkim.rb
@@ -1,7 +1,8 @@
 desc 'gureumkim' do
   latest = github('gureum', 'gureum').first
-  version = latest.tag_name.gsub(/^v/, '')
   latest.assets.select! { |e| e.name.match(/\.pkg$/) }
+  # Sometimes the tag_name doesn't include the whole version string.
+  version = latest.assets.first.name.gsub(/^GureumKIM-|\.pkg$/, '')
   url = latest.assets.first.browser_download_url
   Latest.new version: version, url: url
 end
