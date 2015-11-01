20b1847baf3ff8dbe169b95c07c16bcd2d80cb3e
buo <buo@users.noreply.github.com>
2015-11-01 17:06:53 +0900

    google-refine: update

--- feeds/google-refine.rb
+++ feeds/google-refine.rb
@@ -1,7 +1,7 @@
 desc 'google-refine' do
-  latest = github('OpenRefine', 'OpenRefine').first
-  version = latest.tag_name.gsub(/^v/, '')
+  latest = github('OpenRefine', 'OpenRefine').sort_by { |e| e.tag_name }.last
   latest.assets.select! { |e| e.name.match(/\.dmg$/) }
+  version = latest.assets.first.name.gsub(/^google-refine-|\.dmg$/, '')
   url = latest.assets.first.browser_download_url
   Latest.new version: version, url: url
 end
