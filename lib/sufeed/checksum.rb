676c8977ac384b3ae7bd95f6ac734b43fceb1c75
buo <buo@users.noreply.github.com>
2015-11-01 17:06:11 +0900

    Handle redirection of download url

--- lib/sufeed/checksum.rb
+++ lib/sufeed/checksum.rb
@@ -5,8 +5,11 @@ require 'tempfile'
 module Sufeed
   def self.checksum(url)
     file = Tempfile.new('sufeed')
-    Curl::Easy.download(url, file.path)
-    sha256 = Digest::SHA2.file(file.path).hexdigest
+    redirected = location(url)
+    puts "Redirected to #{redirected}" if url != redirected
+    Curl::Easy.download(redirected, file.path)
+    #sha256 = Digest::SHA2.file(file.path).hexdigest
+    sha256 = Digest::SHA2.hexdigest open(file.path).read
     file.close
     file.unlink
 
