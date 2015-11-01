0b2ccff3883af82d427b81c98baab8eea56805b2
buo <buo@users.noreply.github.com>
2015-11-01 17:26:46 +0900

    Add jd-gui

---
+++ feeds/jd-gui.rb
@@ -0,0 +1,7 @@
+desc 'jd-gui' do
+  latest = github('java-decompiler', 'jd-gui').first
+  version = latest.tag_name.gsub(/^v/, '')
+  latest.assets.select! { |e| e.name.match(/osx/) }
+  url = latest.assets.first.browser_download_url
+  Latest.new version: version, url: url
+end
