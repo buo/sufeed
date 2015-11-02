e63ca09335027a50b87fcfc8dc9bf05660962d02
buo <buo@users.noreply.github.com>
2015-11-02 15:31:08 +0900

    Add launchrocket

---
+++ feeds/launchrocket.rb
@@ -0,0 +1,6 @@
+desc 'launchrocket' do
+  latest = github('jimbojsb', 'launchrocket').first
+  version = latest.tag_name.gsub(/^v/, '')
+  url = latest.assets.first.browser_download_url
+  Latest.new version: version, url: url
+end
