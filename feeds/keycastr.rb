a0868ef298902b5bd7cfea6ba092ee41c536174d
buo <buo@users.noreply.github.com>
2015-11-02 15:23:00 +0900

    Add keycastr

---
+++ feeds/keycastr.rb
@@ -0,0 +1,6 @@
+desc 'keycastr' do
+  latest = github('keycastr', 'keycastr').first
+  version = latest.tag_name#.gsub(/^v/, '')
+  url = latest.assets.first.browser_download_url
+  Latest.new version: version, url: url
+end
