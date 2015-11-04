09d85c141dc443e46fa2cbbb063b78d8df351ad2
buo <buo@users.noreply.github.com>
2015-11-04 15:49:59 +0900

    Add streakerbar

---
+++ feeds/streakerbar.rb
@@ -0,0 +1,6 @@
+desc 'streakerbar' do
+  latest = github('chaserx', 'streakerbar').first
+  version = latest.tag_name.gsub(/^v/, '')
+  url = latest.assets.first.browser_download_url
+  Latest.new version: version, url: url
+end
