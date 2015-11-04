89a41b1a9a9cbc4e80f2a0fba2fa99067402776d
buo <buo@users.noreply.github.com>
2015-11-04 15:49:58 +0900

    Add qlnetcdf

---
+++ feeds/qlnetcdf.rb
@@ -0,0 +1,6 @@
+desc 'qlnetcdf' do
+  latest = github('tobeycarman', 'QLNetcdf').first
+  version = latest.tag_name.gsub(/^v/, '')
+  url = latest.assets.first.browser_download_url
+  Latest.new version: version, url: url
+end
