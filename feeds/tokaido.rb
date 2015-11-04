cb24ddf8a0ee48dbe6ffc1ae28aec0246e2ba5ab
buo <buo@users.noreply.github.com>
2015-11-04 15:49:59 +0900

    Add tokaido

---
+++ feeds/tokaido.rb
@@ -0,0 +1,6 @@
+desc 'tokaido' do
+  latest = github('tokaido', 'tokaidoapp').first
+  version = latest.tag_name.gsub(/^v/, '')
+  url = latest.assets.first.browser_download_url
+  Latest.new version: version, url: url
+end
