f7337180fcda4aee5038dc9666e305ff83186f16
buo <buo@users.noreply.github.com>
2015-11-04 15:49:59 +0900

    Add quotefix

---
+++ feeds/quotefix.rb
@@ -0,0 +1,6 @@
+desc 'quotefix' do
+  latest = github('robertklep', 'quotefixformac').first
+  version = latest.tag_name.gsub(/^v/, '')
+  url = latest.assets.first.browser_download_url
+  Latest.new version: version, url: url
+end
