c27cdae01a975c3228c31e5e4619ab01ab4b23d2
buo <buo@users.noreply.github.com>
2015-11-04 15:49:58 +0900

    Add qlprettypatch

---
+++ feeds/qlprettypatch.rb
@@ -0,0 +1,6 @@
+desc 'qlprettypatch' do
+  latest = github('atnan', 'QLPrettyPatch').first
+  version = latest.tag_name.gsub(/^v/, '')
+  url = latest.assets.first.browser_download_url
+  Latest.new version: version, url: url
+end
