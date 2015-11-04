bff041c1e0b9ec20b745c0895c9a5188c7733982
buo <buo@users.noreply.github.com>
2015-11-04 14:29:28 +0900

    Add sqlitebrowser

---
+++ feeds/sqlitebrowser.rb
@@ -0,0 +1,7 @@
+desc 'sqlitebrowser' do
+  latest = github('sqlitebrowser', 'sqlitebrowser').first
+  version = latest.tag_name.gsub(/^v/, '')
+  latest.assets.select! { |e| e.name.match(/dmg/) }
+  url = latest.assets.first.browser_download_url
+  Latest.new version: version, url: url
+end
