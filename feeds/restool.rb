f4808aeccb003981c2158bb44a7c1a57dbbf9c74
buo <buo@users.noreply.github.com>
2015-11-04 15:49:59 +0900

    Add restool

---
+++ feeds/restool.rb
@@ -0,0 +1,7 @@
+desc 'restool' do
+  latest = github('Nikola-K', 'RESTool').first
+  version = latest.tag_name.gsub(/^v/, '')
+  latest.assets.select! { |e| e.name.match(/osx/) }
+  url = latest.assets.first.browser_download_url
+  Latest.new version: version, url: url
+end
