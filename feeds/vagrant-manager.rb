d3dc8e1c18cd487f5500a8360306ff8bb8f93cf5
buo <buo@users.noreply.github.com>
2015-11-04 14:32:20 +0900

    Add vagrant-manager

---
+++ feeds/vagrant-manager.rb
@@ -0,0 +1,6 @@
+desc 'vagrant-manager' do
+  latest = github('lanayotech', 'vagrant-manager').first
+  version = latest.tag_name.gsub(/^v/, '')
+  url = latest.assets.first.browser_download_url
+  Latest.new version: version, url: url
+end
