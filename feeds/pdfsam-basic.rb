c745509c552abb86a73d7d0cd98b2014a98d7859
buo <buo@users.noreply.github.com>
2015-11-04 15:49:58 +0900

    Add pdfsam-basic

---
+++ feeds/pdfsam-basic.rb
@@ -0,0 +1,7 @@
+desc 'pdfsam-basic' do
+  latest = github('torakiki', 'pdfsam-v2').first
+  version = latest.tag_name.gsub(/^v/, '')
+  latest.assets.select! { |e| e.name.match(/dmg/) }
+  url = latest.assets.first.browser_download_url
+  Latest.new version: version, url: url
+end
