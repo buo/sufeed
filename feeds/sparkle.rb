d66fa339a00b0e570f8633067ce5483eb492ebef
buo <buo@users.noreply.github.com>
2015-11-04 14:28:35 +0900

    Add sparkle

---
+++ feeds/sparkle.rb
@@ -0,0 +1,6 @@
+desc 'sparkle' do
+  latest = github('sparkle-project', 'Sparkle').first
+  version = latest.tag_name.gsub(/^v/, '')
+  url = latest.assets.first.browser_download_url
+  Latest.new version: version, url: url
+end
