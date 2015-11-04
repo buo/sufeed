a3f162c07d414ebb892d15ab5d027620660c46ab
buo <buo@users.noreply.github.com>
2015-11-04 14:38:23 +0900

    Add mac-linux-usb-loader

---
+++ feeds/mac-linux-usb-loader.rb
@@ -0,0 +1,6 @@
+desc 'mac-linux-usb-loader' do
+  latest = github('SevenBits', 'Mac-Linux-USB-Loader').first
+  version = latest.tag_name.gsub(/^v/, '')
+  url = latest.assets.first.browser_download_url
+  Latest.new version: version, url: url
+end
