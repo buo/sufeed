0cb7534df6456c32b433e7f166e0ae6e8571a79b
buo <buo@users.noreply.github.com>
2015-11-04 19:09:50 +0900

    Fix a bug that doesn't follow a redirect chain

--- lib/sufeed/checksum.rb
+++ lib/sufeed/checksum.rb
@@ -5,10 +5,9 @@ require 'tempfile'
 module Sufeed
   def self.checksum(url)
     file = Tempfile.new('sufeed')
-    redirected = location(url)
-    puts "Redirected to #{redirected}" if url != redirected
-    Curl::Easy.download(redirected, file.path)
-
+    Curl::Easy.download(url, file.path) do |curl|
+      curl.follow_location = true
+    end
     sha256 = Digest::SHA2.file(file.path).hexdigest
     return sha256
   end
