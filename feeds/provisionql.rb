7eb8a81a1cf148297ea448cd471a8170307b6643
buo <buo@users.noreply.github.com>
2015-11-04 15:49:58 +0900

    Add provisionql

---
+++ feeds/provisionql.rb
@@ -0,0 +1,6 @@
+desc 'provisionql' do
+  latest = github('ealeksandrov', 'ProvisionQL').first
+  version = latest.tag_name.gsub(/^v/, '')
+  url = latest.assets.first.browser_download_url
+  Latest.new version: version, url: url
+end
