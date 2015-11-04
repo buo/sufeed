94428b263ed5997177919c04c89bb750d96cbe40
buo <buo@users.noreply.github.com>
2015-11-04 15:50:00 +0900

    Add ttscoff-mmd-quicklook

---
+++ feeds/ttscoff-mmd-quicklook.rb
@@ -0,0 +1,6 @@
+desc 'ttscoff-mmd-quicklook' do
+  latest = github('ttscoff', 'mmd-quicklook').first
+  version = latest.tag_name.gsub(/^v/, '')
+  url = latest.assets.first.browser_download_url
+  Latest.new version: version, url: url
+end
