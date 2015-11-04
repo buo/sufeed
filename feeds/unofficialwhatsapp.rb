fdf077db3ec11c80769f68a7b3caac62474b239b
buo <buo@users.noreply.github.com>
2015-11-04 15:50:00 +0900

    Add unofficialwhatsapp

---
+++ feeds/unofficialwhatsapp.rb
@@ -0,0 +1,7 @@
+desc 'unofficialwhatsapp' do
+  latest = github('Aluxian', 'WhatsApp-Desktop').first
+  version = latest.tag_name.gsub(/^v/, '')
+  latest.assets.select! { |e| e.name.match(/dmg/) }
+  url = latest.assets.first.browser_download_url
+  Latest.new version: version, url: url
+end
