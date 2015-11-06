67cf27cd848cbcc0776fbc374e99b04a838407f7
buo <buo@users.noreply.github.com>
2015-11-06 16:50:10 +0900

    qldds.rb: fix parsing error

--- feeds/qldds.rb
+++ feeds/qldds.rb
@@ -1,6 +1,6 @@
 desc 'qldds' do
   latest = github('Marginal', 'QLdds').first
-  version = latest.tag_name.gsub(/^rel-/, '')
+  version = latest.name.gsub(/^Release/, '').strip
   url = latest.assets.first.browser_download_url
   Latest.new version: version, url: url
 end
