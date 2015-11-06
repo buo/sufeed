a17ff5ef47d53a5a35f5991ec4bc21a5d460b2c7
buo <buo@users.noreply.github.com>
2015-11-06 16:45:43 +0900

    pandoc.rb: select only mac packages

--- feeds/pandoc.rb
+++ feeds/pandoc.rb
@@ -1,6 +1,12 @@
 desc 'pandoc' do
-  latest = github('jgm', 'pandoc').first
+  # Sometimes the feed item has only a windows package.
+  items = github('jgm', 'pandoc').select do |item|
+    idx = item.assets.find_index { |e| e.name.match(/osx/) }
+    !idx.nil?
+  end
+  latest = items.first
   version = latest.tag_name.gsub(/^v/, '')
+  latest.assets.select! { |e| e.name.match(/osx/) }
   url = latest.assets.first.browser_download_url
   Latest.new version: version, url: url
 end
