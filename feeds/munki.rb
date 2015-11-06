15c530ef0adc1182ec73226f59f9fca13f3dae25
buo <buo@users.noreply.github.com>
2015-11-06 16:44:33 +0900

    munki.rb: update

--- feeds/munki.rb
+++ feeds/munki.rb
@@ -1,6 +1,7 @@
 desc 'munki' do
   latest = github('munki', 'munki').first
-  version = latest.tag_name.gsub(/^v/, '')
+  #version = latest.tag_name.gsub(/^v/, '')
   url = latest.assets.first.browser_download_url
+  version = url.match(/munkitools-([\d\.]+)\.pkg/)[1]
   Latest.new version: version, url: url
 end
