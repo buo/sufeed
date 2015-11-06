934994d5d4835ccc9357cfb9527aa68acd80cde5
buo <buo@users.noreply.github.com>
2015-11-06 16:46:17 +0900

    sshfs.rb: remove the package name from version string

--- feeds/sshfs.rb
+++ feeds/sshfs.rb
@@ -1,6 +1,6 @@
 desc 'sshfs' do
   latest = github('osxfuse', 'sshfs').first
-  version = latest.tag_name.gsub(/^v/, '')
+  version = latest.tag_name.gsub(/^osxfuse-sshfs-/, '')
   url = latest.assets.first.browser_download_url
   Latest.new version: version, url: url
 end
