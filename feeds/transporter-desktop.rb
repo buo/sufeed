7e094591ec6149cf358396fb6f01d40ff05b1a0a
buo <buo@users.noreply.github.com>
2015-11-18 05:14:30 +0900

    Update transporter-desktop

--- feeds/transporter-desktop.rb
+++ feeds/transporter-desktop.rb
@@ -1,8 +1,15 @@
 desc 'transporter-desktop' do
-  item = sparkle('http://appsoftware.connecteddata.com/mac/2.5/Appcast.xml').last
-  version = item.enclosure.version
-  url = item.enclosure.url
-  build = url.match(/#{version}_([\d]+)\.zip/)[1]
-  version = "#{version}_#{build}"
+  # deprecated for homebrew-cask compatibility.
+  #item = sparkle('http://appsoftware.connecteddata.com/mac/2.5/Appcast.xml').last
+  #version = item.enclosure.version
+  #url = item.enclosure.url
+  #build = url.match(/#{version}_([\d]+)\.zip/)[1]
+  #version = "#{version}_#{build}"
+  
+  # The URL was extracted from the following official web page.
+  # https://secure.connecteddata.com/download/desktop
+  data = jsondata('https://secure.connecteddata.com/get/desktopsoftware?version=latest&os=mac')
+  version = data['url'].match(/Transporter_Desktop_([\d\.\_]+)\.dmg/)[1]
+  url = "https://secure.connecteddata.com#{data['url']}"
   Latest.new version: version, url: url
 end
